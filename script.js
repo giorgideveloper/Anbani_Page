b9a68cb8dff2e4a97b48049ba3df14aedb9126ad
giorgideveloper <gorgiladze888@gmail.com>
2021-12-23 20:54:40 +0400

    dasrulebuli

---
+++ script.js
@@ -0,0 +1,45 @@
+/* const anbani_out = document.querySelector('.anbani_out');
+const img = ['https://gobatumi.com/file-manager/files/shares/anbani/a.jpg','https://gobatumi.com/file-manager/files/shares/anbani/b.jpg','https://gobatumi.com/file-manager/files/shares/anbani/g.jpg','https://gobatumi.com/file-manager/files/shares/anbani/d.jpg','https://gobatumi.com/file-manager/files/shares/anbani/e.jpg','https://gobatumi.com/file-manager/files/shares/anbani/v.jpg','https://gobatumi.com/file-manager/files/shares/anbani/z.jpg','https://gobatumi.com/file-manager/files/shares/anbani/t.jpg','https://gobatumi.com/file-manager/files/shares/anbani/i.jpg',]
+const src = ["https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F607679403909349%2F&show_text=false&width=560&t=0","https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F607679403909349%2F&show_text=false&width=560&t=0","https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0",
+"https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F377786620706934%2F&show_text=false&width=560&t=0"]
+
+const title = ["ანბანის კოშკი","ბოტანიკური ბაღი","გონიოს ციხე","დანდალოს ხიდი","ევროპის მოედანი","ველობილიკი","ზღვა","თაგო","იახნი","კვარიათი","ლაზური მუზეუმი","მაჭახელა","ნობელები"]
+
+function my_card(){
+  for(let i = 0; i < img.length; i++){
+    console.log(img.length)
+    anbani_out.innerHTML = `  
+      <div class="col-md-4">
+          <div class="visit_card">
+          <div class="card" style="width: 100%;  margin-bottom: 1em; margin-top:1em; ">
+              <img src="${img[i]}" class="card-img-top" data-bs-toggle="modal" data-src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fvisitbatumi%2Fvideos%2F607679403909349%2F&show_text=false&width=560&t=0" data-bs-target="#myModal" alt="...">
+              <div class="card-body" >
+                  <br>
+                <h5 class="card-title col-12">ანბანის კოშკი</h5>
+                <p class="card-text col-md-8 col-lg-10"> #გაიცანიაჭარაანბანით #ანბანისკოშკი #ბათუმი</p>
+                <div class="btn_abs col-md-4 col-lg-2"> 
+                <a href="#"><button  class="btn btn-primary my-btn">ა</button></a>
+              </div>
+              </div>
+            </div>
+          </div>
+      </div>`
+      
+  }
+    
+}
+
+my_card();
+
+
+ */
